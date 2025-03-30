6ff4ee824604ae4eb92abc06676ed753c661c9c8
Gandhiji <1nonlygem@gmail.com>
2025-03-30 17:38:39 +0530

    feat: remove missing nav

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -1,8 +1,5 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
-
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const Stack = createStackNavigator();
@@ -12,18 +9,7 @@ export default function AppNavigator() {
     <NavigationContainer>
       <Stack.Navigator>
         {/* Implement lazy loading using the getComponent pattern */}
-        <Stack.Screen 
-          name="Home"
-          getComponent={() => require('../screens/HomeScreen').default}
-          options={{ title: 'Home' }}
-        />
-        <Stack.Screen
-          name="Details"
-          getComponent={() => require('../screens/DetailsScreen').default}
-          options={{ title: 'Details' }}
-        />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
-
